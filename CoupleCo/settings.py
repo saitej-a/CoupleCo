5d1b76e70c6df9be761d3ce753cfbcd3e824111f
saitej-a <134436978+saitej-a@users.noreply.github.com>
2024-01-11 15:24:29 +0530

    Update settings.py

--- CoupleCo/settings.py
+++ CoupleCo/settings.py
@@ -76,9 +76,10 @@ TEMPLATES = [
 
 CHANNEL_LAYERS={
     'default':{
-        'BACKEND':'channels_redis.core.RedisChannelLayer',
-        'CONFIG':{
-        'hosts':[('127.0.0.1',6379)],
+        # 'BACKEND':'channels_redis.core.RedisChannelLayer',
+        # 'CONFIG':{
+        # 'hosts':[('127.0.0.1',6379)],
+        'BACKEND':'channels.layers.InMemoryChannelLayer',
         }
     }
 }
